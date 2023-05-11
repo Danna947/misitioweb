5e6776b65fac0d5981d68e5b0919b79ee6bf9cd3
dannal <dannalopez072004@gmail.com>
2023-05-10 21:10:09 -0500

    actualizacion11

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,10 @@
  let arrow = document.getElementById("arrowleft");
 
  let img = document.getElementById("imghipo");
- let img2= document.getElementById("imgdolphi");
+ let img2= document.getElementById("imgleopard");
 
 
  arrow.addEventListener("click", function(){
     img.classList.remove("hipo")
-    img2.classList.add("dolphi")
+    img2.classList.add("leopard")
  })
