3c212326d2714dff77737ce3eaf779df2d317b15
dannal <dannalopez072004@gmail.com>
2023-05-11 16:08:30 -0500

    actualiza111

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,25 @@
- let arrow = document.getElementById("arrowleft");
-
- let img = document.getElementById("imghipo");
- let img2= document.getElementById("imgleopard");
-
+ let arrow = document.getElementById("arrowLeft");
+ let arrowR= document.getElementById("arrowRigth")
+ let imgSea = document.getElementById("imgSea");
+ let imgLeopard = document.getElementById("imgLeopard");
+ let imgDolphi = document.getElementById("imgDolphi");
 
  arrow.addEventListener("click", function(){
-    img.classList.remove("hipo")
-    img2.classList.add("leopard")
- })
+    if (imgSea.classList.contains("img-sea")) {
+      imgSea.classList.remove("img-sea");
+      imgSea.classList.add("img-sea-hideen");
+      imgLeopard.classList.remove("img-sea-hideen");
+      imgLeopard.classList.add("img-leopard-hideen");
+    }
+});
+arrowR.addEventListener("click",function(){
+   if (imgSea.classList.contains("img-sea")) {
+      imgSea.classList.remove("img-sea");
+      imgSea.classList.add("img-sea-hideen-rigth");
+      imgLeopard.classList.remove("img-sea-hideen");
+      imgLeopard.classList.add("img-leopard-hideen");
+    }
+
+});
+
+
