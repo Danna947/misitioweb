68ceff0c7652da5f3f8cc187d170126732bb05a3
Danna947 <dannalopez072004@gmail.com>
2023-06-20 22:58:00 -0500

    terms and conditions

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,7 @@
  let imgLeopard = document.getElementById("imgLeopard");
  let imgNature = document.getElementById("imgNature");
 
+<<<<<<< HEAD
 
  arrowLeft.addEventListener("click", function(){
    if (imgSea.classList.contains("img-sea")) {
@@ -24,6 +25,40 @@
         imgNature.classList.add("img-nature-hideen");
    
     }
+=======
+arrowRigth.addEventListener("click", function(){
+ if(imgSea.classList.contains("img-sea")){
+    imgSea.classList.remove("img-sea");
+    imgSea.classList.add("img-sea-hideen");
+    imgLeopard.classList.add("img-leopard");
+    imgLeopard.classList.remove("img-leopard-hideen-right");
+
+ }
+ else if(imgLeopard.classList.contains("img-leopard")){
+   imgLeopard.classList.remove("img-leopard");
+   imgLeopard.classList.add("img-leopard-hideen-left");
+   imgNature.classList.add("img-nature");
+   imgNature.classList.remove("img-nature-hideen-right");
+
+ }
+});
+arrowLeft.addEventListener("click", function(){
+ if(imgNature.classList.contains("img-nature")){;
+   imgNature.classList.remove("img-nature");
+   imgNature.classList.add("img-nature-hideen-right");
+   imgLeopard.classList.add("img-leopard");
+   imgLeopard.classList.remove("img-leopard-hideen-left")
+
+ }
+ else if(imgLeopard.classList.contains("img-leopard")){
+   imgLeopard.classList.remove("img-leopard");
+   imgLeopard.classList.add("img-leopard-hideen-right");
+   imgSea.classList.add("img-sea");
+   imgSea.classList.remove("img-sea-hideen");
+
+ }
+ 
+>>>>>>> 2b3c68441b39e8e0a38fa61bb283553b63b3e04a
 });
 arrowRigth.addEventListener("click", function(){
    if (imgNature.classList.contains("img-nature")) {
@@ -48,6 +83,74 @@ arrowRigth.addEventListener("click", function(){
    
 
  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  arrowRigth.addEventListener("click", function(){
+//    if (imgSea.classList.contains("img-sea")) {
+//       imgSea.classList.remove("img-sea");
+//       imgSea.classList.add("img-sea-hideen");
+//       imgLeopard.classList.add("img-nature");
+//       imgLeopard.classList.add("img-nature-hideen");
+      
+//    }
+   //  else if(imgLeopard.classList.contains("img-nature")){
+   //        imgLeopard.classList.remove("img-nature");
+   //      imgLeopard.classList.add("img-nature-hideen");
+   //      imgNature.classList.add("img-leopard");
+   //      imgNature.classList.remove("img-leopard-hideen-rigth")
+   // }
+//     else if(imgNature.classList.contains("img-nature")){
+//          imgNature.classList.remove("img-nature");
+//         imgNature.classList.add("img-nature-hideen");
+   
+//     }
+// });
+// arrowRigth.addEventListener("click", function(){
+//    if (imgNature.classList.contains("img-nature")) {
+//       imgNature.classList.remove("img-nature");
+//       imgNature.classList.add("img-nature-hideen-rigth");
+      
+//    }
+   
+//   else if(imgLeopard.classList.contains("img-leopard")){
+//       imgLeopard.classList.remove("img-leopard");
+//       imgLeopard.classList.add("img-leopard-hideen");
+//       imgSea.classList.add("img-sea");
+//       imgSea.classList.remove("img-sea-hideen-rigth");
+//    }
+//    else if(imgSea.classList.contains("img-sea")){
+//           imgSea.classList.remove("img-sea");
+//          imgSea.classList.add("img-sea-hideen-rigth");
+      
+
+//   }
+  
+   
+
+//  });
 
 
 
