42a4331440e96eee96f4d02f3ddb651a287d1191
Danna947 <dannalopez072004@gmail.com>
2023-08-16 11:40:07 -0500

    actualizacion portafolio 50%

---
+++ js/fuction.js
@@ -0,0 +1,12 @@
+
+ let btnMenu = document.getElementById("btnMenu");
+ let navMenu = document.getElementById("navMenu");
+ let backMenu = document.getElementById("backmenu");
+btnMenu.addEventListener("click",function(){
+    navMenu.style.right = "0px";
+    backMenu.style.display = "block";
+});
+backMenu.addEventListener("click",function(){
+    navMenu.style.right = "-250px";
+    backMenu.style.display = "none";
+})
