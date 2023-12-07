dbc07b4216fe383398e1b15b0fee7287823811d9
Danna947 <dannalopez072004@gmail.com>
2023-12-06 22:56:01 -0500

    actualizaciones

--- js/barras.js
+++ js/barras.js
@@ -1,20 +1,20 @@
 function llenarBarras() {
-    let puntuacion5 = document.getElementById("puntuacion5").value;
-    let puntuacion4 = document.getElementById("puntuacion4").value;
-    let puntuacion3 = document.getElementById("puntuacion3").value;
-    let puntuacion2 = document.getElementById("puntuacion2").value;
-    let puntuacion1 = document.getElementById("puntuacion1").value;
+    let puntuacion5 = parseFloat(document.getElementById("puntuacion5").value) || 0;
+    let puntuacion4 = parseFloat(document.getElementById("puntuacion4").value) || 0;
+    let puntuacion3 = parseFloat(document.getElementById("puntuacion3").value) || 0;
+    let puntuacion2 = parseFloat(document.getElementById("puntuacion2").value) || 0;
+    let puntuacion1 = parseFloat(document.getElementById("puntuacion1").value) || 0;
 
-    let barras = document.querySelectorAll("#starBars progress");
+    let sumaTotal = puntuacion5 + puntuacion4 + puntuacion3 + puntuacion2 + puntuacion1;
 
-    barras[0].value = puntuacion5;
-    barras[1].value = puntuacion4;
-    barras[2].value = puntuacion3;
-    barras[3].value = puntuacion2;
-    barras[4].value = puntuacion1;
+    if (sumaTotal > 0) {
+      let barras = document.querySelectorAll("#starBars progress");
+      let labels = document.querySelectorAll("#starBars .progress-label");
+
+      barras.forEach((bar, index) => {
+        let porcentaje = (eval(`puntuacion${5 - index}`) / sumaTotal) * 100;
+        bar.value = porcentaje;
+        labels[index].textContent = `${porcentaje.toFixed()}%`;
+      });
+    }
   }
-
-
-
-
-
